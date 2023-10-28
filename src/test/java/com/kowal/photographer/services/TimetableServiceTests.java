1a8e7b5ebdd90cbff1f1818591198d6de767d28d
grzegorzakowalski <grzegorz.a.kowalski@gmail.com>
2023-10-28 15:14:53 +0200

    1 more test.

--- src/test/java/com/kowal/photographer/services/TimetableServiceTests.java
+++ src/test/java/com/kowal/photographer/services/TimetableServiceTests.java
@@ -1,7 +1,12 @@
 package com.kowal.photographer.services;
 
+import com.kowal.photographer.entities.Pictures;
 import com.kowal.photographer.entities.Timetable;
+import com.kowal.photographer.entities.User;
+import com.kowal.photographer.pojo.AddPhoto;
+import com.kowal.photographer.repositorys.PicturesRepository;
 import com.kowal.photographer.repositorys.TimetableRepository;
+import com.kowal.photographer.repositorys.UserRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -21,6 +26,15 @@ public class TimetableServiceTests {
     private TimetableRepository mockTimetableRepository;
     @Mock
     private ConfigurationService mockConfigurationService;
+
+    @Mock
+    private UserService mockUserService;
+
+    @Mock
+    private PicturesRepository mockPicturesRepository;
+
+    @Mock
+    private UserRepository mockUserRepository;
 
     @InjectMocks
     private TimetableService timetableService;
@@ -81,5 +95,36 @@ public class TimetableServiceTests {
     public void givenOnlyNotConfirmedDoneList_SearchingForConfirmed_WillReturnEmptyList(){
         Mockito.when(mockTimetableRepository.findAllByConfirmed(true)).thenReturn(new ArrayList<>());
         assertEquals(0, timetableService.getNotDoneListByConfirmed(true).size());
+    }
+
+    @Test
+    public void givenAddPhotoObject_WillSaveCorrectly(){
+        Pictures picture = new Pictures();
+        picture.setLink("www.google.pl");
+        picture.setDescription("Google");
+        User owner = User.builder().id(1L).active(true).role("ROLE_USER").password("yolo").username("foo@baa").phoneNumber("666").pictures(new ArrayList<>()).build();
+        Mockito.when(mockPicturesRepository.save(picture)).thenReturn(picture);
+        Timetable timetable = Timetable.builder()
+                .date(LocalDate.now()).
+                id(1L).confirmed(true).
+                isDone(false).
+                description("opis").
+                hour("11")
+                .owner(owner)
+                .build();
+        User admin = User.builder().id(2L).role("ROLE_ADMIN").pictures(new ArrayList<>()).build();
+        AddPhoto addPhoto = new AddPhoto();
+        addPhoto.setTimetable(timetable);
+        addPhoto.setPictures(picture);
+        Mockito.when(mockUserRepository.save(admin)).thenReturn(admin);
+        Mockito.when(mockUserService.getAdminListWithoutPicture(picture)).thenReturn(List.of(admin));
+        Mockito.when(mockUserRepository.save(owner)).thenReturn(owner);
+        Mockito.when(mockTimetableRepository.save(timetable)).thenReturn(timetable);
+        try{
+            timetableService.addPhoto(addPhoto);
+        } catch (Exception e){
+            fail();
+        }
+
     }
 }
